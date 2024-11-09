3ecb45c69649f4a18b6a66d2fb481823e9fcf642
LMBarboza <leon.uchoa@gmail.com>
2024-11-09 01:50:10 -0300

    add ImageFolder

--- Projeto01/train_models.py
+++ Projeto01/train_models.py
@@ -4,16 +4,15 @@ import configparser
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 from torchvision import datasets, transforms
-
 from modules.rede_factory import RedeFactory
 from modules.trainer import Trainer
 from modules.treino_strategy import STDStrategy
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(description="Treinamento MLP")
+    parser = argparse.ArgumentParser(description="Transfer Learning")
     parser.add_argument(
         "--config",
         type=str,
@@ -38,16 +37,19 @@ def main() -> None:
         device = torch.device("cpu")
 
     transform = transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+        [
+            transforms.Resize((256, 384)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+        ]
+    )
+    dataset = datasets.ImageFolder(
+        root="./data/Wonders of World/Wonders of World/", transform=transform
     )
 
-    train_dataset = datasets.CIFAR10(
-        root="./data", train=True, download=True, transform=transform
-    )
-
-    test_dataset = datasets.CIFAR10(
-        root="./data", train=False, download=True, transform=transform
-    )
+    train_size = int(0.8 * len(dataset))
+    test_size = len(dataset) - train_size
+    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
 
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
@@ -57,24 +59,23 @@ def main() -> None:
     sOutput = 10
 
     accuracy_list = []
-    hiddenLayers = [[32], [32, 64], [32, 64, 128]]
+    hiddenLayers = [32, 64, 128]
 
-    for i, layers in enumerate(hiddenLayers):
-        model = RedeFactory.createRede(
-            sChannels, sOutput, layers, sKernel, fnActivation=nn.ReLU
-        ).to(device)
+    model = RedeFactory.createRede(
+        sChannels, sOutput, hiddenLayers, sKernel, fnActivation=nn.ReLU
+    ).to(device)
 
-        print(model)
+    print(model)
 
-        optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-        loss = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    loss = nn.CrossEntropyLoss()
 
-        strategy = STDStrategy(loss, optimizer)
+    strategy = STDStrategy(loss, optimizer)
 
-        trainer = Trainer(model, strategy)
-        accuracy = trainer.train(train_dataloader, test_dataloader, epochs, device)
-        accuracy_list.append(accuracy)
-        torch.save(model.state_dict(), f"models/model{i}.pth")
+    trainer = Trainer(model, strategy)
+    accuracy = trainer.train(train_dataloader, test_dataloader, epochs, device)
+    accuracy_list.append(accuracy)
+    torch.save(model.state_dict(), "models/model.pth")
 
     with open("results/layers_accuracy.json", "w") as f:
         json.dump(accuracy_list, f)
