88b3ceb480378def555449e60538ee290cbeee1f
LMBarboza <leon.uchoa@gmail.com>
2024-11-18 02:44:28 -0300

    add datasets

--- Projeto01/train_models.py
+++ Projeto01/train_models.py
@@ -28,6 +28,8 @@ def main() -> None:
     learning_rate = config.getfloat("TRAINING", "learning_rate")
     epochs = config.getint("TRAINING", "epochs")
     use_cuda = config.getboolean("TRAINING", "cuda")
+    data = config.get("TRAINING", "dataset")
+    base_model = config.get("TRAINING", "base")
 
     cuda = use_cuda and torch.cuda.is_available()
 
@@ -36,23 +38,54 @@ def main() -> None:
     else:
         device = torch.device("cpu")
 
-    transform = transforms.Compose(
-        [
-            transforms.Resize((256, 384)),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
-        ]
-    )
-    dataset = datasets.ImageFolder(
-        root="./data/Wonders of World/Wonders of World/", transform=transform
-    )
+    match data:
+        case "wow":
 
-    train_size = int(0.8 * len(dataset))
-    test_size = len(dataset) - train_size
-    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+            transform = transforms.Compose(
+                [
+                    transforms.Resize((256, 384)),
+                    transforms.ToTensor(),
+                    transforms.Normalize(
+                        mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)
+                    ),
+                ]
+            )
+            dataset = datasets.ImageFolder(
+                root="./data/Wonders of World/Wonders of World/", transform=transform
+            )
 
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+            train_size = int(0.8 * len(dataset))
+            test_size = len(dataset) - train_size
+            train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+
+            train_dataloader = DataLoader(
+                train_dataset, batch_size=batch_size, shuffle=True
+            )
+            test_dataloader = DataLoader(
+                test_dataset, batch_size=batch_size, shuffle=False
+            )
+
+        case "xray":
+            transform = transforms.Compose(
+                [
+                    transforms.Resize((224, 224)),
+                    transforms.ToTensor(),
+                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+                ]
+            )
+            train_dataset = datasets.ImageFolder(
+                root="./data/chest_xray/train/", transform=transform
+            )
+            test_dataset = datasets.ImageFolder(
+                root="./data/chest_xray/test/", transform=transform
+            )
+
+            train_dataloader = DataLoader(
+                train_dataset, batch_size=batch_size, shuffle=True
+            )
+            test_dataloader = DataLoader(
+                test_dataset, batch_size=batch_size, shuffle=False
+            )
 
     sChannels = 3
     sKernel = 5
